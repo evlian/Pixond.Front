3c455ecedcc28c82b42aa06af35cf7640dd3d0bb
@artanberishaa <artaanberishaa@gmail.com>
2022-05-03 22:34:15 +0200

    Front-End Register

---
+++ Pixond-Front/client-pixond/src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import './App.css'
+import Register from './Register'
+
+function App() {
+
+    return (
+        <main className="App">
+            <Register />
+        </main>
+    )
+}
+
+
+export default App;
